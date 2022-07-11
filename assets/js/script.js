eb696d08062f419f60c10d822f459f97b577880e
Khristopher-Schick <104205888+Khristopher-Schick@users.noreply.github.com>
2022-07-10 22:37:47 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+
+const questionText = [
+    {
+        question: "what is html?",
+        a: "a coding languages",
+        b: "a banana",
+        c: "a very simlar banana",
+        d: "a not so simlar banana",
+        correct: "d",
+    },
+    {
+        question: "what is css",
+        a: "html",
+        b: "not html",
+        c: "almost html",
+        d: "ssc",
+        correct: "b",
+    },
+    {
+        question: "where am i?",
+        a: "apple",
+        b: "in ur room",
+        c: "cheese",
+        d: "appler",
+        correct: "d",
+    },
+    {
+        question: "when?",
+        a: "did",
+        b: "I",
+        c: "Ask",
+        d: "i did ask?",
+        correct: "d",
+    },
+
+
+];
+
